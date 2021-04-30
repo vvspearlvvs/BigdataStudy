3485de774296d0d4ca8f541d2ec621c3560c9805
jinju <gg66477@gmail.com>
2021-04-30 21:37:24 +0900

    update:exception

--- LottoData/main.py
+++ LottoData/main.py
@@ -13,21 +13,44 @@ def load_lotto(round):
 
     return raw_data
 
+def cal_tax(permnt):
+    if permnt >=300000000:
+        tax = 300000000 * 0.22 + (permnt-300000000)*0.33
+    elif permnt<300000000 and permnt>5000:
+        tax= permnt*0.22
+    else:
+        tax = 0
+
+    return round(permnt-tax,0)
+
 def make_data(raw_data):
+    dict_data ={}
 
-    data=pd.DataFrame.from_dict(raw_data,orient='index')
-    data=data.transpose()
+    dict_data['date'] = raw_data['drwNoDate'] #추첨날짜
+    dict_data['winner'] = raw_data['firstPrzwnerCo'] #당첨자 수
+    dict_data['winmnt'] = raw_data['firstWinamnt'] #1등당첨금액
+    winmnt = dict_data['winmnt']
+    dict_data['permnt'] = round(dict_data['winmnt'] / dict_data['winner'],0) #인당 수령액
+    permnt = dict_data['permnt']
+    dict_data['relmnt'] = cal_tax(permnt) #인당 실수령액(세금제외)
 
-    return data
+    pd_data=pd.DataFrame.from_dict(dict_data,orient='index')
+    pd_data=pd_data.transpose()
+
+    return pd_data
 
 def main():
-    for r in range(1,10):
+    for r in range(950,960):
         raw_data = load_lotto(r)
+
         data = make_data(raw_data)
+        print(data)
+        '''
         if data.loc[0,"returnValue"] == "fail":
             break
         else:
             print(data)
+        '''
 
 if __name__ == '__main__':
     main()
