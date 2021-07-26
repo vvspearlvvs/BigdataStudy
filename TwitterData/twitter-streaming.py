b2baeece8d5d650304c71a9d92ca303679b51196
jinju <gg66477@gmail.com>
2021-07-26 23:29:46 +0900

    add:extract data

--- TwitterData/twitter-streaming.py
+++ TwitterData/twitter-streaming.py
@@ -3,31 +3,43 @@ import json
 from pymongo import MongoClient
 import requests_oauthlib
 import tqdm
-
+import pandas as pd
+from pyspark.sql import SparkSession
 
 
 mongodb_host = "localhost"
 mongodb_port = "27017"
 
-def main():
+mongo_client = MongoClient(mongodb_host, int(mongodb_port))
+database = mongo_client.get_database('mydb')
+collection = database.get_collection('twitter')
+mongodb_uri = "mongodb://localhost/mydb.twitter"
+
+def load_data():
     twitter = requests_oauthlib.OAuth1Session(consumer_key,consumer_secret,access_token_key,access_token_secret)
     uri='https://stream.twitter.com/1.1/statuses/sample.json'
     respone=twitter.get(uri,stream=True)
-    print(respone.status_code)
-    #print(r.raise_for_status().code())
 
-    mongo_client = MongoClient(mongodb_host, int(mongodb_port))
-    database = mongo_client.get_database('mydb')
-    collection = database.get_collection('twitter')
+    if respone.status_code == '200':
+        # mongodb connect
 
-    print(respone.iter_lines())
-    for line in tqdm.tqdm(respone.iter_lines(),unit='tweets',mininterval=1):
-        if line:
-            tweet = json.loads(line)
-            print(tweet)
-            #tweet['_timestamp']=datetime.datetime.utcnow().isoformat()
-            #collection.insert_one(tweet)
+        for line in tqdm.tqdm(respone.iter_lines(),unit='tweets',mininterval=1):
+            if line:
+                tweet = json.loads(line)
+                tweet['_timestamp']=datetime.datetime.utcnow().isoformat()
+                # mongodb insert
+                collection.insert_one(tweet)
 
+def extract_data(*args, **kwargs):
+
+    # mongodb extract
+    for tweet in collection.find(*args, **kwargs):
+        if 'delete' not in tweet:
+            yield{
+                'created_at':tweet['created_at'],'text':tweet['text']
+            }
 
 if __name__ == "__main__":
-    main()
+    load_data()
+    df= pd.DataFrame(extract_data({'lang':'ko'},limit=10))
+    print(df)
